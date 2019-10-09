ca968c9870d94bbf132b35981d79db130f0e36a9
Kinza Kanwal <48015166+kinzakanwal@users.noreply.github.com>
2019-10-09 11:56:22 +0500

    Type casting from double to int

---
+++ type casting from double to int.cpp
@@ -0,0 +1,13 @@
+//typecasting from double to int
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	double x=9.8;
+//	cout<<fixed<<setprecision(3);
+//	static_cast<int>(x);
+	int y=x;	
+	cout<<y;
+		
+}
