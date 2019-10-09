385a3365c56a12a705cc718650c774d8d99c909a
Kinza Kanwal <48015166+kinzakanwal@users.noreply.github.com>
2019-10-09 11:59:34 +0500

    Even odd checker

---
+++ even odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter a number"<<endl;
+	cin>>n;
+	if(n%2)
+	cout<<"the number "<<n<< " is odd"<<endl;
+	else
+	cout<<"the number "<<n<<" is even"<<endl;
+	
+	
+	return 0;
+}
