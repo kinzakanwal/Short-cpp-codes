e9091e86d428114c0e527bd87f0b91826e006324
Kinza Kanwal <48015166+kinzakanwal@users.noreply.github.com>
2019-10-09 11:54:46 +0500

    Prints ASCII value of a character

---
+++ ASCII value of character.cpp
@@ -0,0 +1,18 @@
+//ASCII value of characer
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{int a='b';
+//int a;
+
+//int x=9;
+//int a=x;
+//static_cast<double>(x);
+//static_cast<int>(9.8);
+//cout<<fixed;
+//cout<<setprecision(2);
+cout<<a;
+return 0;
+
+}
